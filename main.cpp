2168b5bba3db91175dbf7c5aee2c62463ee463a6
jordlynn <snake.river52@gmail.com>
2015-07-10 15:15:23 -0700

    First push

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+
+using namespace std;
+struct jason{
+	int children;
+	String names[];
+}Jason;
+
+void AddChildren(String name);
+
+int main(){
+
+	return 0;
+}
+
+void AddChildre(String name){
+
+}
